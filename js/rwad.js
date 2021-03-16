c48cfcf641f868844bcc253042c6836f7b9214f1
Lezo <hi@caolei.info>
2021-03-16 18:45:57 +0800

    Create rwad.js

---
+++ js/rwad.js
@@ -0,0 +1,1 @@
+document.writeln("<a href=\'https://defirex.org/?utm_source=0xzx\' rel=\'noreferrer nofollow external noopener\'><img class=\'alignnone size-full\' src=\'https://0xzx.com/img/ads/300x250.gif\' alt=\'Defirex\' width=\'300\' height=\'250\' /></a>");
