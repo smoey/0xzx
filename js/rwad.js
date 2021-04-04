4eb1b6bbe0082a1d1fd238b5460141c7e488a5f8
Lezo <hi@caolei.info>
2021-04-05 02:45:10 +0800

    Update rwad.js

--- js/rwad.js
+++ js/rwad.js
@@ -1,1 +1,68 @@
-document.writeln("<a href=\'https://token.0xzx.com/#buy-token\' rel=\'noreferrer nofollow external noopener\'><img class=\'alignnone size-full\' src=\'https://cdn.jsdelivr.net/gh/smoey/0xzx@main/img/swap0xzx.jpg\' alt=\'Defirex\' width=\'300\' height=\'250\' /></a>");
+<script>
+info="<!DOCTYPE html>
+<html>
+    <head>
+    	<style>
+.buybtc {
+    border: 1px solid #c6fbe7;
+    padding: 5px 10px 5px 10px;
+    margin: 10px 15px;
+    line-height: 50px;
+    border-radius: 3px;
+    box-shadow: 0 0 17px rgb(224 224 224 / 49%);
+    background: url(https://0xzx.com/img/coinzilla-fav.png) no-repeat #fff;
+    background-position: top 7px left 3px;
+}
+.buybtc:hover {
+	background: url(https://0xzx.com/img/coinzilla-fav.png) no-repeat #000;
+    background-position: top 7px left 3px;
+}
+.swap {
+    font-size: 18px;
+    font-weight: 700;
+    color: #757884;
+    padding-left: 25px;
+}
+.swapad {
+    text-shadow: none;
+    text-align: center;
+    max-width: 300px;
+    background: #fff6f6;
+}
+
+.swap:hover {
+    font-weight: 700;
+    color:#f75876;
+    font-style: italic;
+}
+.contract {
+    display: block;
+    width: 300px;
+    background: #edfff5;
+    font-size: 12px;
+    font-family: cursive;
+}
+.social {
+
+	font-size: 12px;
+    font-family: cursive;
+}
+.contract-title {
+
+}
+a {
+    text-decoration: none!important;
+}
+    	</style>
+    </head>
+	<body>
+<div class=&#34;swapad&#34;>
+<img src=&#34;https://0xzx.com/img/cake.png&#34; style=&#34;height: 50px;float: left;line-height: 50px;&#34;><a href=&#34;https://token.0xzx.com/#buy-token&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34; ><span class=&#34;buybtc swap&#34;>SWAP 0XZX TOKEN</span></a>
+<div class=&#34;contract&#34;><span class=&#34;contract-title&#34;>BSC:</span><a href=&#34;https://bscscan.com/token/0x1fbb6ca220dcbe732f796fa9b13dd21cd654511b&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34;>0x1fbb6ca220dcbe732f796fa9b13dd21cd654511b</a></div>
+<span class=&#34;social&#34;><a href=&#34;https://t.me/chat0x&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34;>TELEGRAM: @chat0x</a></span> ~ <span class=&#34;social&#34;><a href=&#34;https://twitter.com/0xzxcom&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34;>TWITTER: @0xzxcom</a></span>
+</div>
+	</body>
+</html>"
+
+document.write(info)
+</script>
