8b9384a6981188b750aa1c5a05e7b18d1c6b3887
Lezo <hi@caolei.info>
2021-04-05 02:51:42 +0800

    Update rwad.js

--- js/rwad.js
+++ js/rwad.js
@@ -1,64 +1,59 @@
-<script>
-info="
-    	<style>
-.buybtc {
-    border: 1px solid #c6fbe7;
-    padding: 5px 10px 5px 10px;
-    margin: 10px 15px;
-    line-height: 50px;
-    border-radius: 3px;
-    box-shadow: 0 0 17px rgb(224 224 224 / 49%);
-    background: url(https://0xzx.com/img/coinzilla-fav.png) no-repeat #fff;
-    background-position: top 7px left 3px;
-}
-.buybtc:hover {
-	background: url(https://0xzx.com/img/coinzilla-fav.png) no-repeat #000;
-    background-position: top 7px left 3px;
-}
-.swap {
-    font-size: 18px;
-    font-weight: 700;
-    color: #757884;
-    padding-left: 25px;
-}
-.swapad {
-    text-shadow: none;
-    text-align: center;
-    max-width: 300px;
-    background: #fff6f6;
-}
-
-.swap:hover {
-    font-weight: 700;
-    color:#f75876;
-    font-style: italic;
-}
-.contract {
-    display: block;
-    width: 300px;
-    background: #edfff5;
-    font-size: 12px;
-    font-family: cursive;
-}
-.social {
-
-	font-size: 12px;
-    font-family: cursive;
-}
-.contract-title {
-
-}
-a {
-    text-decoration: none!important;
-}
-    	</style>
-
-<div class=&#34;swapad&#34;>
-<img src=&#34;https://0xzx.com/img/cake.png&#34; style=&#34;height: 50px;float: left;line-height: 50px;&#34;><a href=&#34;https://token.0xzx.com/#buy-token&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34; ><span class=&#34;buybtc swap&#34;>SWAP 0XZX TOKEN</span></a>
-<div class=&#34;contract&#34;><span class=&#34;contract-title&#34;>BSC:</span><a href=&#34;https://bscscan.com/token/0x1fbb6ca220dcbe732f796fa9b13dd21cd654511b&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34;>0x1fbb6ca220dcbe732f796fa9b13dd21cd654511b</a></div>
-<span class=&#34;social&#34;><a href=&#34;https://t.me/chat0x&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34;>TELEGRAM: @chat0x</a></span> ~ <span class=&#34;social&#34;><a href=&#34;https://twitter.com/0xzxcom&#34; rel=&#34;nofollow external noopener noreferrer&#34; target=&#34;_blank&#34;>TWITTER: @0xzxcom</a></span>
-</div>
-"
-
-document.write(info)
-</script>
+document.write("<style>");
+document.write(".buybtc {");
+document.write("    border: 1px solid #c6fbe7;");
+document.write("    padding: 5px 10px 5px 10px;");
+document.write("    margin: 10px 15px;");
+document.write("    line-height: 50px;");
+document.write("    border-radius: 3px;");
+document.write("    box-shadow: 0 0 17px rgb(224 224 224 \/ 49%);");
+document.write("    background: url(https:\/\/0xzx.com\/img\/coinzilla-fav.png) no-repeat #fff;");
+document.write("    background-position: top 7px left 3px;");
+document.write("}");
+document.write(".buybtc:hover {");
+document.write("	background: url(https:\/\/0xzx.com\/img\/coinzilla-fav.png) no-repeat #000;");
+document.write("    background-position: top 7px left 3px;");
+document.write("}");
+document.write(".swap {");
+document.write("    font-size: 18px;");
+document.write("    font-weight: 700;");
+document.write("    color: #757884;");
+document.write("    padding-left: 25px;");
+document.write("}");
+document.write(".swapad {");
+document.write("    text-shadow: none;");
+document.write("    text-align: center;");
+document.write("    max-width: 300px;");
+document.write("    background: #fff6f6;");
+document.write("}");
+document.write("");
+document.write(".swap:hover {");
+document.write("    font-weight: 700;");
+document.write("    color:#f75876;");
+document.write("    font-style: italic;");
+document.write("}");
+document.write(".contract {");
+document.write("    display: block;");
+document.write("    width: 300px;");
+document.write("    background: #edfff5;");
+document.write("    font-size: 12px;");
+document.write("    font-family: cursive;");
+document.write("}");
+document.write(".social {");
+document.write("");
+document.write("	font-size: 12px;");
+document.write("    font-family: cursive;");
+document.write("}");
+document.write(".contract-title {");
+document.write("");
+document.write("}");
+document.write("a {");
+document.write("    text-decoration: none!important;");
+document.write("}");
+document.write("<\/style>");
+document.write("");
+document.write("<div class=\"swapad\">");
+document.write("<img src=\"https:\/\/0xzx.com\/img\/cake.png\" style=\"height: 50px;float: left;line-height: 50px;\"><a href=\"https:\/\/token.0xzx.com\/#buy-token\" rel=\"nofollow external noopener noreferrer\" target=\"_blank\" ><span class=\"buybtc swap\">SWAP 0XZX TOKEN<\/span><\/a>");
+document.write("<div class=\"contract\"><span class=\"contract-title\">BSC:<\/span><a href=\"https:\/\/bscscan.com\/token\/0x1fbb6ca220dcbe732f796fa9b13dd21cd654511b\" rel=\"nofollow external noopener noreferrer\" target=\"_blank\">0x1fbb6ca220dcbe732f796fa9b13dd21cd654511b<\/a><\/div>");
+document.write("<span class=\"social\"><a href=\"https:\/\/t.me\/chat0x\" rel=\"nofollow external noopener noreferrer\" target=\"_blank\">TELEGRAM: @chat0x<\/a><\/span> ~ <span class=\"social\"><a href=\"https:\/\/twitter.com\/0xzxcom\" rel=\"nofollow external noopener noreferrer\" target=\"_blank\">TWITTER: @0xzxcom<\/a><\/span>");
+document.write("<\/div>");
+document.write("");
